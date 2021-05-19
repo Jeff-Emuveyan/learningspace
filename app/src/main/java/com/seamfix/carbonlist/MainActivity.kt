d802dbcda6ba3f7aab5eb07c847f7ec7976d625f
jeffemuveyan <jemuveyan@seamfix.com>
2021-05-19 01:24:16 +0100

    Init

---
+++ app/src/main/java/com/seamfix/carbonlist/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.seamfix.carbonlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seamfix.features.Dog
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ features/src/main/java/com/seamfix/features/Dog.kt
@@ -0,0 +1,4 @@
+package com.seamfix.features
+
+class Dog {
+}
