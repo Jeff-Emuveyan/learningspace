812aaec19e1455dd8581ff622b6d1dfd069e3e95
jeffemuveyan <jemuveyan@seamfix.com>
2021-05-20 11:08:59 +0100

    minor update.

--- core/src/main/java/com/seamfix/core/source/local/AppDataBase.kt
+++ core/src/main/java/com/seamfix/core/source/local/AppDataBase.kt
@@ -21,13 +21,6 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        /*** Before creating the database, we have to make sure that it is encrypted. We use SQLCipher
-         * to accomplish this.
-         * Useful links: https://medium.com/@sonique6784/protect-your-room-database-with-sqlcipher-on-android-78e0681be687
-         * https://stackoverflow.com/questions/62770394/is-android-room-database-secure-to-store-sensitive-data
-         * https://github.com/sqlcipher/android-database-sqlcipher#using-sqlcipher-for-android-with-room
-         * https://medium.com/vmware-end-user-computing/securing-a-room-database-with-passcode-based-encryption-82ec670961e
-         */
         fun getDatabase(context: Context): AppDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
