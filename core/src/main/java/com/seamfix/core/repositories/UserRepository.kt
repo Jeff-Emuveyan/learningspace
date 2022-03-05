dadcdd6c032e8ebea20f977931b0b73c7912df9b
jemuveyan <jemuveyan@axxess.com>
2022-03-05 05:36:03 +0100

    commit

--- core/src/main/java/com/seamfix/core/repositories/UserRepository.kt
+++ core/src/main/java/com/seamfix/core/repositories/UserRepository.kt
@@ -4,8 +4,10 @@ import com.seamfix.core.model.table.UserEntity
 import com.seamfix.core.source.local.dao.UserDao
 import com.seamfix.core.source.remote.Service
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -25,5 +27,5 @@ open class UserRepository @Inject constructor(private val ioDispatcher: Coroutin
             val random = Math.random()
             emit(UserEntity(firstName = "Jeff--${random}"))
         }
-    }
+    }.flowOn(Dispatchers.IO) // We use this since our fragment is using Dispatchers.MAIN
 }
