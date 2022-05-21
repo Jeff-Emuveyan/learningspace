81c6c9dfcd2450b0114c94f9df0b42e1c2a24801
jemuveyan <jemuveyan@axxess.com>
2022-05-21 17:13:02 +0100

    update

--- uitests-hilt/src/androidTest/java/com/example/uitests_hilt/ListOfCitiesFragmentInstrumentedTest.kt
+++ uitests-hilt/src/androidTest/java/com/example/uitests_hilt/ListOfCitiesFragmentInstrumentedTest.kt
@@ -1,40 +1,54 @@
 package com.example.uitests_hilt
 
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.uitests_hilt.hilt.AppDatabaseModule
 import com.example.uitests_hilt.ui.list.ListOfCitiesFragment
+import com.example.uitests_hilt.ui.list.ListOfCitiesViewModel
 import com.example.uitests_hilt.util.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-
+import dagger.hilt.android.testing.UninstallModules
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+
+/***
+ * I used mockk instead of mockito-android because mockito-android threw errors when I tried to mock
+ * methods and it outright fell apart when I tried to dependency inject a mock by attaching @BindValue @JvmField
+ */
 
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class ListOfCitiesFragmentInstrumentedTest {
 
-    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @BindValue @JvmField
+    val viewModel:ListOfCitiesViewModel = mockk<ListOfCitiesViewModel>(relaxed = true)
 
     @Before
     fun setUp() {
         // Populate @Inject fields in test class
         hiltRule.inject()
+        launchFragmentInHiltContainer<ListOfCitiesFragment>()
     }
 
     @Test
-    fun test_that_info_text_is_shown() {
-        val scenario = launchFragmentInHiltContainer<ListOfCitiesFragment>()
+    fun test_that_proper_network_error_message_is_show_when_there_is_network_error() {
 
-        assertEquals("com.example.uitests_hilt", appContext.packageName)
+        onView(withId(R.id.tvSearch)).perform(typeText("Hello Jeff"))
+        onView(withText("paul is bad")).check(matches(isDisplayed()))
     }
 }
