f74c8a150b84f7c0624faa9ef487ca83b87a50ce
LAPTOP-PHAJIQ3D\Jeff <jeffemuveyan@gmail.com>
2022-09-03 16:41:01 +0100

    more yml update

--- uitests-hilt/src/androidTest/java/com/example/uitests_hilt/ExampleInstrumentedTest.kt
+++ uitests-hilt/src/androidTest/java/com/example/uitests_hilt/ExampleInstrumentedTest.kt
@@ -20,6 +20,11 @@ class ExampleInstrumentedTest {
 
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.uitests_hilt", appContext.packageName)
+
+        var name = "Jeff"
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S){
+            name = "Fred"
+        }
+        assertEquals("Jeff", name)
     }
 }
